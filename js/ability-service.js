729448a4ea653b33d9f770bac8be60bb84aef060
erictmielke <69402770+erictmielke@users.noreply.github.com>
2026-09-11 21:48:35 -0500

    adding abilities with onesignal 2

--- js/ability-service.js
+++ js/ability-service.js
@@ -42,7 +42,6 @@ export async function castAbility(abilityId, target = {}, variables = {}) {
   const { data: { session } } = await client.auth.getSession();
   if (!session?.user) throw new Error('You must be signed in.');
   const { data: unlocked } = await client.from('user_abilities').select('ability_id').eq('user_id', session.user.id).eq('ability_id', abilityId).maybeSingle();
-  if (ability.unlockRequirement && !unlocked) throw new Error('This ability is locked.');
   const { data, error } = await client.rpc('cast_ability', { ability_id_value: abilityId, target_user_id_value: target.userId || null, target_camp_value: target.camp || null, target_npc_value: target.npc || null, target_location_value: target.location || null, variables_value: variables });
   if (error) throw error;
   return { ...data, ability: getAbility(abilityId) || { id: abilityId } };
